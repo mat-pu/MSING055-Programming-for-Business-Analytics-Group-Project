a1b6013f5f6177d1caff004020d51514f3864952
mat-pu <32641389+mat-pu@users.noreply.github.com>
2017-11-21 00:21:20 +0000

    Update twitter_scraper_csv.py

--- twitter_scraper_csv.py
+++ twitter_scraper_csv.py
@@ -12,32 +12,19 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-# Open/Create a CSV file to append data:
-outfile = open('Brexit1.csv', 'a')
-
-# Use CSV writer to impute data to the CSV file:
-csv_write = csv.writer(outfile)
-
-# Create header row for our data:
-csv_write.writerow(["tweet_id",
-                    "username",
-                    "number_of_followers",
-                    "created_at",
-                    "tweet_text",
-                    "source",
-                    "likes",
-                    "retweets",
-                    "user_location"])
-
-# Search restricted to 1000 most recent tweets in English containing the hashtag 'brexit'
-for tweet in tweepy.Cursor(api.search, q="#brexit", lang="en").items(1000):
-     csv_write.writerow([tweet.id,
-                         tweet.author.screen_name,
-                         tweet.author.followers_count,
-                         tweet.created_at,
-                         tweet.text,
-                         tweet.source,
-                         tweet.favorite_count,
-                         tweet.retweet_count,
-                         tweet.author.location])
-outfile.close()
+# Open CSV file to append new rows (i.e., the actual data):
+with open('Brexit_data.csv', 'a') as outfile:
+  # Searching a selected number of tweets (e.g., 100) about a specific topic in connection to brexit (e.g., stock market)
+  # Restriction on date/period
+    for tweet in tweepy.Cursor(api.search, q="brexit AND <search term>",lang="en",since = "2017-11-14").items(100):
+      # Using CSV writer to inpute new rows with data:
+        csv_write = csv.writer(outfile, delimiter=',', quotechar='"')
+        csv_write.writerow([tweet.id, 
+                        tweet.author.screen_name, 
+                        tweet.author.followers_count, 
+                        tweet.created_at, 
+                        tweet.text, 
+                        tweet.source, 
+                        tweet.favorite_count, 
+                        tweet.retweet_count, 
+                        tweet.author.location])
