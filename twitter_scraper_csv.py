584a0ab575e73b950911dd63e109404cda1dddb5
mat-pu <32641389+mat-pu@users.noreply.github.com>
2017-11-27 09:46:47 +0000

    Update twitter_scraper_csv.py

--- twitter_scraper_csv.py
+++ twitter_scraper_csv.py
@@ -11,13 +11,15 @@ access_token_secret = 'ACCESS_SECRET'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-
-# Open CSV file to append new rows with data (based on the query):
-with open('Brexit_data.csv', 'a') as outfile:
-  # Searching a selected number of tweets (e.g., 100) about a specific topic in connection to brexit (e.g., stock market)
-  # Restriction on date/period
-    for tweet in tweepy.Cursor(api.search, q="brexit AND <search term>",lang="en",since="2017-11-11", until="2017-11-18").items(100):
-      # Using CSV writer to inpute new rows with data:
+        
+# Open CSV file to append new rows with data (based on the query):        
+with open('Brexit_NEW.csv', 'a') as outfile:
+  # Searching a selected number of tweets (e.g., 1000) about a specific keyword in connection to Brexit (e.g., unemployment)
+  # Restriction on date/period & language
+    for tweet in tweepy.Cursor(api.search, q="brexit AND <keyword>",
+                               lang="en",
+                               since="2017-11-15",
+                               until="2017-11-20").items(1000):
         csv_write = csv.writer(outfile, delimiter=',', quotechar='"')
         csv_write.writerow([tweet.id, 
                         tweet.author.screen_name, 
